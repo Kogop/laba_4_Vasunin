6c63b5c20557139bed9fd4169a8b785da7c27d6d
Nesterov Vlad <nesterovvlad100@gmail.com>
2021-12-09 13:18:01 +0300

    finished

--- laba_4_Vasunin/Source.cpp
+++ laba_4_Vasunin/Source.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 
-int Mas[10] = { 10,3,-2,3,23,1,1,1,9,2 };
+int Mas[10] = { 10,3,-2,3,-23,1,1,1,9,2 };
 int Mas1[10];
 int isMax() {
 
@@ -22,28 +22,40 @@ int isMax() {
 	}
 
 
-	Mas1[0] = Mas[0];
-	Mas1[1] = Mas[1];
-	Mas1[2] = Mas[2];
-
-
-	for (int i = 0; i < 10; i++)
+	if (Mas[0]*Mas[1]*Mas[2]>=Mas[0]*Mas[9]*Mas[8])
 	{
-		if (Mas1[0] <= Mas[i])
-		{
-			Mas1[0] = Mas[i];
-		}
-		else if (Mas1[1] <= Mas[i])
-		{
-			Mas1[1] = Mas[i];
-		}
-		else if (Mas1[2] < Mas[i]) 
-		{
-			Mas1[2] = Mas[i];
-		}
+		cout << Mas[2] << " " << Mas[1] << " " << Mas[0] << endl;
+	}
+	else
+	{
+		cout << Mas[0] << " " << Mas[9] << " " << Mas[8] << endl;
 	}
 
-	cout << Mas1[0] << " " << Mas1[1] << " " << Mas1[2] << endl;
+
+
+
+	//Mas1[0] = Mas[0];
+	//Mas1[1] = Mas[1];
+	//Mas1[2] = Mas[2];
+
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	if (Mas1[0] <= Mas[i])
+	//	{
+	//		Mas1[0] = Mas[i];
+	//	}
+	//	else if (Mas1[1] <= Mas[i])
+	//	{
+	//		Mas1[1] = Mas[i];
+	//	}
+	//	else if (Mas1[2] < Mas[i]) 
+	//	{
+	//		Mas1[2] = Mas[i];
+	//	}
+	//}
+
+	//cout << Mas1[0] << " " << Mas1[1] << " " << Mas1[2] << endl;
 
 
 
